a00a3490f861704824b7b47a8dafb9a47fd78e5a
Go Watanabe <1570475+wtnbgo@users.noreply.github.com>
2026-03-31 17:26:21 +0900

    update for no windows: src/plugins/ncbind

--- ncbind.cpp
+++ ncbind.cpp
@@ -14,6 +14,8 @@
 
 #define EXPORT(hr) extern "C" DLL_EXPORT hr STDCALL
 
+#ifdef _WIN32
+
 #ifdef _MSC_VER
 # if defined(_M_AMD64) || defined(_M_X64)
 #  pragma comment(linker, "/EXPORT:V2Link")
@@ -32,7 +34,6 @@ asm (".ascii \" -export:V2Link=V2Link@4 -export:V2Unlink=V2Unlink@0\"");
 # endif
 #endif
 
-#ifdef _WIN32
 //--------------------------------------
 HINSTANCE gDllInstance = NULL;
 
